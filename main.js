e7dd8e1ff5bef26d6c3702db85166d1fdefab5fe
HP <thecodemaster26@gmail.com>
2023-09-08 22:31:57 +0600

    Added Game status and Github repo link

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
 const buttons = document.querySelectorAll('.userBtn')
 const computer = document.getElementById('computerChoise');
 const user = document.getElementById('userChoise');
-const comScore = document.getElementById('comScore');
-const urScore = document.getElementById('urScore');
-let computerScore = 0;
-let userScore =0;
-
-
-
-// btn.forEach(bt => {
-//     bt.addEventListener('click', (e)=>{
-//         const vael = e.target.innerHTML
-//         console.log(vael);
-//     })
-// })
+const result = document.getElementById('result');
 
 function computerChose() {
     const computerOptions = ['Rock', 'Paper', 'Scissor']
@@ -24,7 +12,6 @@ function computerChose() {
 buttons.forEach(button=>{
     button.addEventListener('click',(e)=>{
         let userDesition = e.target.innerHTML
-        // console.log(userClick);
         computer.innerHTML = ''
         user.innerHTML = ''
         let el = document.createElement('i')
@@ -48,62 +35,56 @@ buttons.forEach(button=>{
             el2.classList.add('fa-regular', 'fa-hand-back-fist', 'fa-shake','yellow')
             el.classList.add('yellow')
             computer.appendChild(el2)
+            result.innerHTML = 'Its a Draw..!🙂'
         }
         else if (computerDesition === 'Rock' && userDesition === 'Paper'){
             el2.classList.add('fa-regular', 'fa-hand-back-fist', 'fa-shake', 'red')
             el.classList.add('green')
             computer.appendChild(el2)
-            computerScore--
-            userScore++
+            result.innerHTML = 'You Win..!🥳'
         }
         else if (computerDesition === 'Rock' && userDesition === 'Scissor'){
             el2.classList.add('fa-regular', 'fa-hand-back-fist', 'fa-shake', 'green')
             el.classList.add('red')
             computer.appendChild(el2)
-            computerScore++
-            userScore--
+            result.innerHTML = 'You lose..!🥺'
         }
         else if (computerDesition === 'Paper' && userDesition === 'Paper'){
             el2.classList.add('fa-regular', 'fa-hand', 'fa-shake', 'yellow')
             el.classList.add('yellow')
             computer.appendChild(el2)
+            result.innerHTML = 'Its a Draw..!🙂'
         }
         else if (computerDesition === 'Paper' && userDesition === 'Rock'){
             el2.classList.add('fa-regular', 'fa-hand', 'fa-shake', 'green')
             el.classList.add('red')
             computer.appendChild(el2)
-            computerScore++
-            userScore--
+            result.innerHTML = 'You lose..!🥺'
         }
         else if (computerDesition === 'Paper' && userDesition === 'Scissor'){
             el2.classList.add('fa-regular', 'fa-hand', 'fa-shake', 'red')
             el.classList.add('green')
             computer.appendChild(el2)
-            computerScore--
-            userScore++
+            result.innerHTML = 'You Win..!🥳'
         }
         else if (computerDesition === 'Scissor' && userDesition === 'Scissor'){
             el2.classList.add('fa-regular', 'fa-hand-scissors', 'fa-shake', 'yellow')
             el.classList.add('yellow')
             computer.appendChild(el2)
+            result.innerHTML = 'Its a Draw..!🙂'
         }
         else if (computerDesition === 'Scissor' && userDesition === 'Rock'){
             el2.classList.add('fa-regular', 'fa-hand-scissors', 'fa-shake', 'red')
             el.classList.add('green')
             computer.appendChild(el2)
-            computerScore--
-            userScore++
+            result.innerHTML = 'You Win..!🥳'
         }
         else if (computerDesition === 'Scissor' && userDesition === 'Paper'){
             el2.classList.add('fa-regular', 'fa-hand-scissors', 'fa-shake', 'green')
             el.classList.add('red')
             computer.appendChild(el2)
-            computerScore++
-            userScore--
+            result.innerHTML = 'You lose..!🥺'
         }
-
-        comScore.innerHTML = `Computer Score: ${computerScore}`
-        urScore.innerHTML = `User Score: ${userScore}`
 
         // let el = document.createElement('i')
         // let el2 = document.createElement('i')
